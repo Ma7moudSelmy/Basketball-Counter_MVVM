0a3397ec960cff80f8b2ded5fef4b7fe3faaf8a4
MahmoudSelmy <Ma7moud.m.selmy1@gmail.com>
2025-11-16 20:13:49 +0200

    Refactor CounterCubit and CounterState classes; update dependencies in pubspec.lock

--- lib/Cubit/Counter_Cubit.dart
+++ lib/Cubit/Counter_Cubit.dart
@@ -1,5 +1,9 @@
+import 'package:basketball_app/Cubit/Counter_State.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
-class CounterCubit extends Cubit {
-  CounterCubit(super.initialState);
+class CounterCubit extends Cubit<CounterState> {
+  CounterCubit() : super(CounterAIncrementState());
+  int teamAPoints = 0;
+  int teamBPoints = 0;
+  void TreamAIncrement(int ButtonNumber) {}
 }
